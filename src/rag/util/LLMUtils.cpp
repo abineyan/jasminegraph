e8844cb5af1b8606d738e5697301d8b685144522
ParameswaranSajeenthiran <psajeendran@gmail.com>
2026-06-23 17:45:19 +0530

    Replaced CURL_SSLVERSION_DEFAULT to CURL_SSLVERSION_TLSv1_2

--- src/rag/util/LLMUtils.cpp
+++ src/rag/util/LLMUtils.cpp
@@ -74,7 +74,7 @@ std::string callLLM(const std::string& prompt, const std::string& host, const st
     }
 
     // TLS + connection settings
-    curl_easy_setopt(curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_DEFAULT);
+    curl_easy_setopt(curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2);
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
     curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 2L);
     curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
